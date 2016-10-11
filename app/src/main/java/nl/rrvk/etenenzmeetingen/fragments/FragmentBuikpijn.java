cc888a44f7910d617117dddb362356d27f182d73
DESKTOP-51Q94M0\Robert <rvankammen@live.nl>
2016-10-11 14:45:57 +0200

    Nu altijd oude buikpijn melding afmaken

--- app/src/main/java/nl/rrvk/etenenzmeetingen/fragments/FragmentBuikpijn.java
+++ app/src/main/java/nl/rrvk/etenenzmeetingen/fragments/FragmentBuikpijn.java
@@ -10,13 +10,13 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.Calendar;
+import java.util.List;
 
 import nl.rrvk.etenenzmeetingen.MainActivity;
 import nl.rrvk.etenenzmeetingen.R;
 import nl.rrvk.etenenzmeetingen.listeners.DateOnClickListeners;
 import nl.rrvk.etenenzmeetingen.listeners.TimeOnClickListener;
 import nl.rrvk.etenenzmeetingen.model.MeldingenBuikpijn;
-import nl.rrvk.etenenzmeetingen.model.MeldingenPoep;
 import nl.rrvk.etenenzmeetingen.utils.ActivityUtils;
 
 /**
@@ -46,20 +46,31 @@ public class FragmentBuikpijn extends Fragment implements View.OnClickListener {
         return view;
     }
 
+    private void checkOldRegistration() {
+        List<MeldingenBuikpijn> meldingenBuikpijn = MeldingenBuikpijn.find(MeldingenBuikpijn.class, "end_Date = ?", "");
+        if (meldingenBuikpijn.size()>0){
+            melding = meldingenBuikpijn.get(meldingenBuikpijn.size()-1);
+            startDateText.setText(meldingenBuikpijn.get(meldingenBuikpijn.size()-1).startDate);
+            startTimeText.setText(meldingenBuikpijn.get(meldingenBuikpijn.size()-1).startTime);
+            commentsText.setText(meldingenBuikpijn.get(meldingenBuikpijn.size()-1).comments);
+        }
+    }
+
     @Override
     public void onResume() {
         super.onResume();  // Always call the superclass method first
-        int i = 0;
-        if (melding != null) i = 1;
-        updateTimeAndDate(i);
+        checkOldRegistration();
+        updateTimeAndDate();
     }
 
-    private void updateTimeAndDate(int i) {
+    private void updateTimeAndDate() {
         Calendar c = Calendar.getInstance();
-        if (i == 0) {
+        if (melding == null) {
             startDateText.setText(c.get(Calendar.DAY_OF_MONTH) + "-" + c.get(Calendar.MONTH) + "-" + c.get(Calendar.YEAR));
             startTimeText.setText(String.format("%02d:%02d", c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE)));
         } else {
+            startDateText.setText(melding.startDate);
+            startTimeText.setText(melding.startTime);
             endDateText.setText(c.get(Calendar.DAY_OF_MONTH) + "-" + c.get(Calendar.MONTH) + "-" + c.get(Calendar.YEAR));
             endTimeText.setText(String.format("%02d:%02d", c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE)));
         }
@@ -81,7 +92,9 @@ public class FragmentBuikpijn extends Fragment implements View.OnClickListener {
         this.saveButton = (Button) view.findViewById(R.id.buikpijnSave);
 
         saveButton.setOnClickListener(this);
-        updateTimeAndDate(0);
+
+        checkOldRegistration();
+        updateTimeAndDate();
     }
 
     private boolean checkEndDateAndTime() {
@@ -143,6 +156,9 @@ public class FragmentBuikpijn extends Fragment implements View.OnClickListener {
                     // save this part of the melding
                     melding.save();
                     Toast.makeText(this.getContext(), getString(R.string.notification_buikpijn_add_start), Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(this.getContext(), getString(R.string.notification_buikpijn_add_start_fail), Toast.LENGTH_SHORT).show();
                 }
             }
         } else {
